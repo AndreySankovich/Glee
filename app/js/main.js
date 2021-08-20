ffe0e588a807f99692d86b52733b7002298b0f00
AndreySankovich <5640166fd@gmail.com>
2021-08-20 22:07:06 +0300

    start product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function () {
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.01,
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+
+
+  });
+
   var mixer = mixitup('.product__items');
 
   $('.top-slider__inner').slick({
