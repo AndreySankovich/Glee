caf6303d0bcf46f15634c382745bd852cebdcce8
AndreySankovich <5640166fd@gmail.com>
2021-08-20 13:20:16 +0300

    added login page

--- gulpfile.js
+++ gulpfile.js
@@ -32,8 +32,8 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
@@ -73,6 +73,7 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
+  watch(['app/css/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
